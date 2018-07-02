dd03c579ba7624d8914b642294e8fe4b6f8a0350
Scott Chamberlain <myrmecocystus@gmail.com>
2018-07-02 16:17:45 -0700

    fix #11, make classiers in setup.py a list instead of a tuple

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
   install_requires = ['requests>=2.19.0'],
   python_requires  = '>=3.5',
   tests_require    = ["pytest", "pytest-cov"],
-  classifiers      = (
+  classifiers      = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Science/Research',
     'Intended Audience :: Developers',
@@ -43,5 +43,5 @@ setup(
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6'
-	)
+	]
 )
